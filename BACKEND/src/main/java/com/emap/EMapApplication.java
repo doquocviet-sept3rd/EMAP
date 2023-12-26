c87de1fe879c03b513ca18e35a837926c6436b66
Do Quoc Viet <doquocviet.sept3rd@gmail.com>
2023-12-26 23:18:07 +0700

    EMAP: Add backend configuration.

---
+++ BACKEND/src/main/java/com/emap/EMapApplication.java
@@ -0,0 +1,13 @@
+package com.emap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EMapApplication.class, args);
+    }
+
+}
---
+++ BACKEND/src/test/java/com/emap/EMapApplicationTests.java
@@ -0,0 +1,15 @@
+package com.emap;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class EMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
